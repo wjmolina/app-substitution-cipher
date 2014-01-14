a68357deb635310fceca85364a64833ee9b9f59e
Josué <molina.wilfredo.j@gmail.com>
2014-01-14 16:33:02 -0600

    One tab is replaced with four spaces.

--- Simple Substitution Cipher Breaker/Simple Substitution Cipher Breaker/main.cpp
+++ Simple Substitution Cipher Breaker/Simple Substitution Cipher Breaker/main.cpp
@@ -14,190 +14,190 @@ std::unordered_map<std::string, std::list<std::string>> dictionary;
 
 std::string normalize(std::string s)
 {
-	auto C = 'A';
-	auto i = s.begin();
+    auto C = 'A';
+    auto i = s.begin();
 
-	for (auto c : s)
-	{
-		if (std::isupper(c))
-		{
-			continue;
-		}
+    for (auto c : s)
+    {
+        if (std::isupper(c))
+        {
+            continue;
+        }
 
-		std::replace(i++, s.end(), c, C++);
-	}
+        std::replace(i++, s.end(), c, C++);
+    }
 
-	return s;
+    return s;
 }
 
 std::list<std::string> get_candidates(std::string &s)
 {
-	return std::list<std::string>(dictionary[normalize(s)]);
+    return std::list<std::string>(dictionary[normalize(s)]);
 }
 
 struct cstring
 {
-	cstring(std::string w)
-	{
-		word = w;
-		candidates = dictionary[normalize(word)];
+    cstring(std::string w)
+    {
+        word = w;
+        candidates = dictionary[normalize(word)];
 
-		for (unsigned i = 0; i < word.length(); i++)
-		{
-			for (auto j : candidates)
-			{
-				letter_map[word[i]].emplace(j[i]);
-			}
-		}
-	}
+        for (unsigned i = 0; i < word.length(); i++)
+        {
+            for (auto j : candidates)
+            {
+                letter_map[word[i]].emplace(j[i]);
+            }
+        }
+    }
 
-	std::string word;
-	std::list<std::string> candidates;
-	std::unordered_map<char, std::set<char>> letter_map;
+    std::string word;
+    std::list<std::string> candidates;
+    std::unordered_map<char, std::set<char>> letter_map;
 };
 
 std::set<char> intersect(std::set<char> &a, std::set<char> &b)
 {
-	std::set<char> c;
-	std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::inserter(c, c.begin()));
-	return c;
+    std::set<char> c;
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::inserter(c, c.begin()));
+    return c;
 }
 
 std::string apply_map(std::string &cipher, std::string &plain, std::unordered_map<char, char> &map)
 {
-	std::string result = "";
+    std::string result = "";
 
-	for (unsigned i = 0; i < cipher.length(); i++)
-	{
-		result += map.find(cipher[i]) == map.end() ? plain[i] : map[cipher[i]];
-	}
+    for (unsigned i = 0; i < cipher.length(); i++)
+    {
+        result += map.find(cipher[i]) == map.end() ? plain[i] : map[cipher[i]];
+    }
 
-	return result;
+    return result;
 }
 
 std::unordered_map<char, char> update_map(std::string &cipher, std::string &plain, std::unordered_map<char, char> map)
 {
-	for (unsigned i = 0; i < cipher.length(); i++)
-	{
-		if (map.find(cipher[i]) == map.end())
-		{
-			map[cipher[i]] = plain[i];
-		}
-	}
+    for (unsigned i = 0; i < cipher.length(); i++)
+    {
+        if (map.find(cipher[i]) == map.end())
+        {
+            map[cipher[i]] = plain[i];
+        }
+    }
 
-	return map;
+    return map;
 }
 
 bool is_consistent(std::unordered_map<char, char> &map)
 {
-	std::unordered_set<char> counter;
+    std::unordered_set<char> counter;
 
-	for (auto &i : map)
-	{
-		counter.insert(i.second);
-	}
+    for (auto &i : map)
+    {
+        counter.insert(i.second);
+    }
 
-	return map.size() == counter.size();
+    return map.size() == counter.size();
 }
 
 void unscramble(std::vector<cstring> &cipher_words, int depth, std::unordered_map<char, char> map)
 {
-	if (depth >= cipher_words.size())
-	{
-		for (auto &i : cipher_words)
-		{
-			for (auto &j : i.word)
-			{
-				std::cout << map[j];
-			}
+    if (depth >= cipher_words.size())
+    {
+        for (auto &i : cipher_words)
+        {
+            for (auto &j : i.word)
+            {
+                std::cout << map[j];
+            }
 
-			std::cout << ' ';
-		}
+            std::cout << ' ';
+        }
 
-		std::cout << std::endl;
-	}
-	else
-	{
-		for (auto &i : cipher_words[depth].candidates)
-		{
-			if (apply_map(cipher_words[depth].word, i, map) == i && is_consistent(map))
-			{
-				unscramble(cipher_words, depth + 1, update_map(cipher_words[depth].word, i, map));
-			}
-		}
-	}
+        std::cout << std::endl;
+    }
+    else
+    {
+        for (auto &i : cipher_words[depth].candidates)
+        {
+            if (apply_map(cipher_words[depth].word, i, map) == i && is_consistent(map))
+            {
+                unscramble(cipher_words, depth + 1, update_map(cipher_words[depth].word, i, map));
+            }
+        }
+    }
 }
 
 int main()
 {
-	// Initializing the dictionary.
+    // Initializing the dictionary.
 
-	std::ifstream file("EnglishDictionary.txt");
-	std::string word;
+    std::ifstream file("EnglishDictionary.txt");
+    std::string word;
 
-	file >> word;
+    file >> word;
 
-	while (file)
-	{
-		dictionary[normalize(word)].push_back(word);
-		file >> word;
-	}
+    while (file)
+    {
+        dictionary[normalize(word)].push_back(word);
+        file >> word;
+    }
 
-	file.close();
+    file.close();
 
-	// Preparing the ciphertext.
+    // Preparing the ciphertext.
 
-	std::string sentence = "g kyi oaf ca wacrys sytytkys bah ia pylsoxi e nfknigifigac lgxbys";
-	std::istringstream iss(sentence);
-	std::vector<cstring> cipher_words
-	{
-		std::istream_iterator<std::string> { iss },
-		std::istream_iterator<std::string> { }
-	};
+    std::string sentence = "g kyi oaf ca wacrys sytytkys bah ia pylsoxi e nfknigifigac lgxbys";
+    std::istringstream iss(sentence);
+    std::vector<cstring> cipher_words
+    {
+        std::istream_iterator<std::string> { iss },
+        std::istream_iterator<std::string> { }
+    };
 
-	// Preparing to prune.
+    // Preparing to prune.
 
-	std::unordered_map<char, std::set<char>> pruner;
+    std::unordered_map<char, std::set<char>> pruner;
 
-	for (char i = 'a'; i <= 'z'; i++)
-	{
-		for (char j = 'a'; j <= 'z'; j++)
-		{
-			pruner[i].insert(j);
-		}
-	}
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+        for (char j = 'a'; j <= 'z'; j++)
+        {
+            pruner[i].insert(j);
+        }
+    }
 
-	for (auto &i : cipher_words)
-	{
-		for (auto &j : i.word)
-		{
-			pruner[j] = intersect(pruner[j], i.letter_map[j]);
-		}
-	}
+    for (auto &i : cipher_words)
+    {
+        for (auto &j : i.word)
+        {
+            pruner[j] = intersect(pruner[j], i.letter_map[j]);
+        }
+    }
 
-	for (auto &i : cipher_words)
-	{
-		for (unsigned j = 0; j < i.word.length(); j++)
-		{
-			for (auto k = i.candidates.begin(); k != i.candidates.end();)
-			{
-				if (pruner[i.word[j]].find((*k)[j]) == pruner[i.word[j]].end())
-				{
-					k = i.candidates.erase(k);
-				}
-				else
-				{
-					k++;
-				}
-			}
-		}
-	}
+    for (auto &i : cipher_words)
+    {
+        for (unsigned j = 0; j < i.word.length(); j++)
+        {
+            for (auto k = i.candidates.begin(); k != i.candidates.end();)
+            {
+                if (pruner[i.word[j]].find((*k)[j]) == pruner[i.word[j]].end())
+                {
+                    k = i.candidates.erase(k);
+                }
+                else
+                {
+                    k++;
+                }
+            }
+        }
+    }
 
-	// Breaking the ciphertext.
+    // Breaking the ciphertext.
 
-	unscramble(cipher_words, 0, std::unordered_map<char, char>());
+    unscramble(cipher_words, 0, std::unordered_map<char, char>());
 
-	std::cin.get();
+    std::cin.get();
 
-	return 0;
+    return 0;
 }
